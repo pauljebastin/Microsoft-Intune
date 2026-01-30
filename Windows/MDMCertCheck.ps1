067992d02378d869999518d77ebad359878df6c7
PaulPush <jebastin.paul@gmail.com>
2026-01-30 13:35:18 +0000

    Test

--- Windows/MDMCertCheck.ps1
+++ Windows/MDMCertCheck.ps1
@@ -3,7 +3,7 @@
 $MDMCertificate = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Issuer -eq "Microsoft Intune MDM Device CA"}
 
 if($MDMCertificate){
-    Write-Host "Found Valid MDM Intune Certificate in $($env:ComputerName)"
+    Write-Host "Found Valid MDM Intune Certificate in $($env:ComputerName)."
 }else{
     Write-Host "Intune Certificate not found."
 }
